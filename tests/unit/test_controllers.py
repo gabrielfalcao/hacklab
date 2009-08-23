ab9a352fc7472b61938ac6ff242d5660a0408cdf
Gabriel Falcão <gabriel@nacaolivre.org>
2009-08-23 20:02:55 -0300

    fixing unit tests

--- tests/unit/test_controllers.py
+++ tests/unit/test_controllers.py
@@ -1,25 +1,10 @@
 # -*- coding: utf-8 -*-
-
-from mox import Mox
-from nose.tools import assert_equals
+import cherrypy
+from nose.tools import assert_raises
 from hacklab import controllers
 
 class TestHackLab:
     def test_index_calls_render_html(self):
-        mox = Mox()
-
-        mox.StubOutWithMock(controllers, 'template')
-
-        controllers.template.render_html('index.html'). \
-                    AndReturn('supposed-to-be-the-rendered-html')
 
         ctrl = controllers.HackLabController()
-
-        mox.ReplayAll()
-        try:
-            expected_return = 'supposed-to-be-the-rendered-html'
-            got = ctrl.index()
-            assert_equals(got, expected_return)
-            mox.VerifyAll()
-        finally:
-            mox.UnsetStubs()
+        assert_raises(cherrypy.HTTPRedirect, ctrl.index)
